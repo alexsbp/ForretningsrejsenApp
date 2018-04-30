26f9c6b5851b08d535bd7b7d6c13d9e6a3673a39
alexsbp <alex1546@student.eadania.dk>
2018-04-30 11:41:22 +0200

    slettet igen

--- app/src/main/java/com/example/alex/forretningsrejsenapp/MainActivity.java
+++ app/src/main/java/com/example/alex/forretningsrejsenapp/MainActivity.java
@@ -5,14 +5,11 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button bt1;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //laver ændring med sourcetree
-        bt1 = new Button();
 
     }
 }
