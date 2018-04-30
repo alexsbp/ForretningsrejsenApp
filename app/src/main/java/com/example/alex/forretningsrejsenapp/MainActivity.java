6f14feb0b09194cd22407557e0347f4e5c2ecdaf
alexsbp <alex1546@student.eadania.dk>
2018-04-30 11:40:29 +0200

    test

--- app/src/main/java/com/example/alex/forretningsrejsenapp/MainActivity.java
+++ app/src/main/java/com/example/alex/forretningsrejsenapp/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.alex.forretningsrejsenapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //laver ændring med sourcetree
+        bt1 = new Button();
+
     }
 }
